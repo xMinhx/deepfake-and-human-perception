1d583f18c5b3fda7fa1f1fcdb9f6ef8bcc7aed6b
Minh Truong <minh-truong@live.de>
2020-12-23 17:33:09 +0100

    Updated Database and models in Python

--- Classification/models.py
+++ Classification/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from Start.models import User
+from Start.models import Class
+from Start.models import Difficulty
+
 
 # Create your models here.
+
+
+class Video(models.Model):
+    video_id = models.TextField(primary_key=True)
+    difficulty = models.ForeignKey(Difficulty, on_delete=models.PROTECT)
+    video_link_id = models.TextField(unique=True)
+
+    class Meta:
+        managed = False
+        db_table = 'video'
+
+
+class Classification(models.Model):
+    session_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_id = models.ForeignKey(Video, on_delete=models.CASCADE)
+    class_field = models.ForeignKey(Class, on_delete=models.PROTECT)
+    difficulty = models.ForeignKey(Difficulty, on_delete=models.PROTECT)
+    play_pause = models.IntegerField()
+    replay = models.IntegerField()
+    fullscreen = models.IntegerField()
+    playback = models.IntegerField()
+    duration_in_sec = models.IntegerField()
+    text = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'classification'
+        unique_together = (("session_id", "video_id"),)
--- Start/models.py
+++ Start/models.py
@@ -1,4 +1,54 @@
 from django.db import models
+from django.contrib.sessions.models import Session
+
 
 # Create your models here.
 
+
+class Gender(models.Model):
+    label_name = models.TextField(unique=True)
+    label_id = models.IntegerField(primary_key=True)
+
+    class Meta:
+        managed = False
+        db_table = 'gender'
+
+
+class Difficulty(models.Model):
+    label_id = models.IntegerField(primary_key=True)
+    label_name = models.TextField(unique=True)
+
+    class Meta:
+        managed = False
+        db_table = 'difficulty'
+
+
+class Class(models.Model):
+    label_id = models.IntegerField(primary_key=True)
+    label_name = models.TextField(unique=True)
+
+    class Meta:
+        managed = False
+        db_table = 'class_label'
+
+class Testgroup(models.Model):
+    label_id = models.IntegerField(primary_key=True)
+    label_name = models.TextField(unique=True)
+
+    class Meta:
+        managed = False
+        db_table = 'testgroup'
+
+
+class User(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    gender = models.ForeignKey(Gender, on_delete=models.PROTECT)
+    testgroup = models.ForeignKey(Testgroup, on_delete=models.PROTECT)
+    age = models.IntegerField()
+    pixel_width = models.IntegerField()
+    pixel_height = models.IntegerField()
+    fps = models.DecimalField(max_digits=4, decimal_places=3)
+
+    class Meta:
+        managed = False
+        db_table = 'user'
