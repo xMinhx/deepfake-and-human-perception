3882a4e62836b784f410e1a334bafb1032601a26
Minh Truong <minh-truong@live.de>
2021-01-07 00:01:31 +0100

    - Security testing

--- Website/settings.py
+++ Website/settings.py
@@ -27,11 +27,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '***REMOVED***'
-
+SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*"]
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 # Application definition
 
