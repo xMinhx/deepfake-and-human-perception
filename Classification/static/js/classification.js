d8a52841f6e1b332530c4cc265a9a16ee2448b04
Minh <minh-truong@live.de>
2021-02-13 20:12:55 +0100

    Added comments to the code

--- Classification/static/js/classification.js
+++ Classification/static/js/classification.js
@@ -1,20 +1,26 @@
+//Hide display element (progress)
 document.getElementById("display").style.visibility = "hidden";
 
+//Reload video
 function reload_video() {
 	document.getElementById("video").load()
 }
 
+//Show display element (progress)
 function call_display() {
 	document.getElementById("display").style.visibility = "visible";
 }
+//Show progress bar after 2.5 sec
 setTimeout(call_display, 2500);
 var progress_value = parseInt(parseInt(progress) / 25 * 100) + "%";
 document.getElementById("progress_bar").style.width = progress_value;
 document.getElementById("display").innerHTML = progress_value;
 
+//Define variable for feedback
 var correct_screen = document.getElementById("feedback_background_correct");
 var wrong_screen = document.getElementById("feedback_background_wrong");
 
+//Hide both feedback at the beginning
 correct_screen.style.visibility = "hidden";
 wrong_screen.style.visibility = "hidden";
 
@@ -22,6 +28,7 @@ const video = document.getElementById("video");
 var toggle_counter = 0;
 var toggle_play = false;
 
+//Function for play/pause button
 function togglePlay() {
 	if (toggle_play) {
 		video.pause();
@@ -34,6 +41,7 @@ function togglePlay() {
 	toggle_counter++;
 }
 
+//Function for fullscreen button
 function toggleFullscreen() {
 	if (document.fullscreenElement || document.webkitFullscreenElement ||
 		document.mozFullScreenElement) {
@@ -58,8 +66,10 @@ function toggleFullscreen() {
 var fullscreen_counter = 0;
 var video_margin = document.getElementById("video-margin");
 
+//Define event listener for fullscreen integrity, calls check_screen method on fullscreen change
 document.addEventListener("fullscreenchange", check_screen);
 
+//Keeps integrity of fullscreen even after "Esc" has been pressed
 function check_screen() {
 	fullscreen_counter++;
 	if (!(document.fullscreenElement || document.webkitFullscreenElement ||
@@ -75,6 +85,7 @@ function check_screen() {
 
 var replay_counter = 0;
 
+//Function for replay button
 function replay() {
 	replay_counter++;
 	video.currentTime = '0';
@@ -86,6 +97,7 @@ var playback_counter = 0;
 
 var playback_index = 0;
 
+//Function for playback button
 function playback() {
 	playback_index++;
 	if (playback_index > 4) {
@@ -98,12 +110,14 @@ function playback() {
 }
 
 var duration_in_sec = 0;
-
+//Counts duration spent on website per video
 function second_counter() {
 	duration_in_sec += 1;
 }
+//Calls the second_counter function every second
 setInterval(second_counter, 1000)
 
+//If window finished loading hide loading screen
 window.onload = function () {
 	var loader = document.getElementById("loader_screen");
 	loader.style.display = "none";
@@ -111,14 +125,17 @@ window.onload = function () {
 var difficulty = "0";
 var category = "0";
 
+//Submission difficulty of participant
 function set_difficulty(diffi) {
 	difficulty = diffi;
 }
 
+//Submission category of participant
 function set_category(cate) {
 	category = cate;
 }
 
+//Color change of submit button on mouse hover
 document.getElementById("submit_button").onmouseover = function () {
 	var color = "rgb(191,0,0)"
 	if (difficulty !== "0" && category !== "0") {
@@ -127,10 +144,12 @@ document.getElementById("submit_button").onmouseover = function () {
 	this.style.backgroundColor = color;
 };
 
+//Color change of submit button on mouse remove
 document.getElementById("submit_button").onmouseout = function () {
 	this.style.backgroundColor = "rgba(220, 160, 140, 0)";
 };
 
+//Is called if pressed on the submit button, checks integrity of submission and the feedback group
 function check_values() {
 	document.getElementById("display").style.visibility = "hidden";
 	if (["1", "2", "3"].includes(difficulty) && ["1", "2"].includes(category)) {
@@ -148,6 +167,7 @@ function check_values() {
 	}
 }
 
+//Submits the data to the server
 function submit_data() {
 	document.getElementById('play_count').value = toggle_counter;
 	document.getElementById('fullscreen_count').value = fullscreen_counter;
