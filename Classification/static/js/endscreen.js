aec71a2579aea698230d3924b575647f8b798185
Minh <minh-truong@live.de>
2021-02-13 20:14:02 +0100

    Added comments

--- Classification/static/js/endscreen.js
+++ Classification/static/js/endscreen.js
@@ -1,3 +1,4 @@
+//Change the sentences at the beginning to the dictionary values provided by the server
 document.getElementById("p1").innerText = "Your stats, " + dictionary["username"];
 document.getElementById("p2").innerText = "Earned points: " + dictionary["user_score"] + "/2500";
 document.getElementById("p3").innerText = "Correct answer : " + parseInt(dictionary["correct"]/25*100) + "%";
