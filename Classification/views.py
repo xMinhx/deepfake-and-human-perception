3ab75b17e1b570b285cebf692d390ac1be174c58
Minh <minh-truong@live.de>
2021-02-13 20:29:31 +0100

    Added comments

--- Classification/views.py
+++ Classification/views.py
@@ -3,12 +3,17 @@ from Classification.models import Video, Classification
 from Start.models import User, Class, Difficulty, Scoreboard
 import random as rd
 
-
+#Is called when user classified all videos
 def call_end(request):
+    #Check if user exist in database
     if (not request.session.exists(request.session.session_key)) or (
             not User.objects.filter(session_id=request.session.session_key).exists()):
         return render(request, "error page.html")
+    
+    #Get the user instance by session key
     user = User.objects.get(session_id=request.session.session_key)
+    
+    #Calls all instance of correct classification data set from the user
     correct_count = len(Classification.objects.raw('''SELECT c.id, c.session_id_id, c.video_id_id
                                                   FROM public.classification c
                                                   JOIN public.user u ON u.id = c.session_id_id 
@@ -21,8 +26,11 @@ def call_end(request):
     #                                               JOIN public.user u ON u.id = c.session_id_id
     #                                               AND u.id = %s
     #                                               JOIN video v ON v.video_id = c.video_id_id''', [user.id]))
+    
+    #Calls the scoreboard from database
     user_scores = Scoreboard.objects.get(id=1).scores
 
+    #If user doesen't exist in scoreboard add him
     if user.id not in user_scores:
         user_scores.update({str(user.id):
                                 {"username": "User " + str(user.id),
@@ -32,13 +40,16 @@ def call_end(request):
 
     return render(request, "endscreen.html", {"user_score": user_scores[str(user.id)]})
 
-
+#Is called upon entering the classification process
 def classification_view(request):
     if (not request.session.exists(request.session.session_key)) or (
             not User.objects.filter(session_id=request.session.session_key).exists()):
         return render(request, "error page.html")
-
+    
+    #Get instance of the user based on his session key
     user = User.objects.get(session_id=request.session.session_key)
+    
+    #List of all videos used for classification
     video_liste_all = [
         "abarnvbtwb.mp4",
         "akvmwkdyuv.mp4",
@@ -71,12 +82,17 @@ def classification_view(request):
 
     # for i in range(len(video_all_queryset)):
     # video_liste_all.append(video_all_queryset[i][0])
-
+    
+    #Is called if the submit button has been pressed
     if request.method == "POST":
         video_object = Video.objects.get(video_id=request.POST.get("video_id"))
         user_object = User.objects.get(session_id=request.session.session_key)
+        
+        #Check if user already classified the video if yes, call new video
         if Classification.objects.filter(session_id=user_object, video_id=video_object).exists():
             return redirect("classification_name")
+        
+        #Generate new classification dataset based on the information submitted by the user and the front-end
         classification = Classification(
             session_id=user_object,
             video_id=video_object,
@@ -91,7 +107,10 @@ def classification_view(request):
         )
         classification.save()
 
+    #Choose random video from all videos
     selected_video = rd.choice(video_liste_all)
+    
+    #If user already classified atleast one video, calculate the difference and choose a video from that list
     if Classification.objects.filter(session_id=user).exists():
 
         video_liste_user = []
@@ -113,13 +132,17 @@ def classification_view(request):
     return render(request, "classification.html",
                   {"video": video, "videoleft": 25 - len(video_liste_all), "testid": user.testgroup.label_id})
 
-
+#Is called if user visits scoreboard
 def scores(request):
+    #Check if user already exist
     if (not request.session.exists(request.session.session_key)) or (
             not User.objects.filter(session_id=request.session.session_key).exists()):
         return render(request, "error page.html")
-
+    
+    #Calls scoreboard from database
     user_scores = Scoreboard.objects.get(id=1).scores
+    
+    #Sorts the scorboard based on correct answers DESC
     sorted_tuple = sorted(user_scores.items(), key=lambda x: x[1]['correct'], reverse=True)
     sorted_dict = {}
     for x in sorted_tuple:
